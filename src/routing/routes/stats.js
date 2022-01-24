5af980e44f5f6b42570cc5af0ced479f6ac90dfa
Ben Hall <ben@benh.codes>
2022-01-24 18:09:24 +0000

    Took into account inactive guilds
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/routing/routes/stats.js
+++ src/routing/routes/stats.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 // eslint-disable-next-line no-unused-vars
 router.get('/', async (request, response, next) => {
-	const guildCount = await Guild.count();
-	const userCount = await GuildUser.count();
+	const guildCount = await Guild.count({ where: { active: true } });
+	const userCount = await GuildUser.count({ where: { '$guild.active$': true }, include: [{ model: Guild, required: false }] });
 	const whitelabelBotCount = await WhitelabelBot.count();
 
 	return response.status(200).json({
