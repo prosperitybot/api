9d6a2ed9082d5caa9e25c596e8f58c3a75e45758
Ben Hall <ben@benh.codes>
2022-01-27 17:04:13 +0000

    Added querying to the leaderboard
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/routing/routes/leaderboard.js
+++ src/routing/routes/leaderboard.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const { Guild, GuildUser, User } = require('@prosperitybot/database');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 
 router.get('/:id', async (request, response, next) => {
 	const guildId = request.params.id;
-	const { page, limit } = request.query;
+	const { page, limit, q } = request.query;
 
 	const queryLimit = limit == null ? 50 : parseInt(limit) > 50 ? 50 : parseInt(limit);
 	const queryPage = page ?? 1;
 
 	const guild = await Guild.findByPk(guildId);
-	const users = await GuildUser.findAll({
-		where: {
-			guildId: guildId,
-		},
-		offset: queryLimit * (queryPage - 1),
-		limit: parseInt(queryLimit),
-		order: [ ['xp', 'DESC'] ],
-		include: User,
-	});
+	let users;
 
+	if (q != null && q != '') {
+		users = await GuildUser.findAll({
+			where: {
+				'$user.username$': { [Op.substring]: q },
+				guildId: guildId,
+			},
+			offset: queryLimit * (queryPage - 1),
+			limit: parseInt(queryLimit),
+			order: [ ['xp', 'DESC'] ],
+			include: User,
+		});
+	}
+	else {
+		users = await GuildUser.findAll({
+			where: {
+				guildId: guildId,
+			},
+			offset: queryLimit * (queryPage - 1),
+			limit: parseInt(queryLimit),
+			order: [ ['xp', 'DESC'] ],
+			include: User,
+		});
+	}
 	if (!users || !guild) return next(new Error('Guild not found'));
 
 	return response.status(200).json({
